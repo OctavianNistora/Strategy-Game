ac2d699f2c5e3f30ff044762f8fce6764dc7b9d1
Alin402 <alexandru.ionica@student.upt.ro>
2024-11-28 21:32:55 +0200

    some change

--- Api/src/main/java/com/example/api/ApiApplication.java
+++ Api/src/main/java/com/example/api/ApiApplication.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class ApiApplication {
 
     public static void main(String[] args) {
+        System.out.println("dada");
         SpringApplication.run(ApiApplication.class, args);
     }
 
