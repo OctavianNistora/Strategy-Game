dc71064f99bdac2da5ce86a0f44c048448366f5d
Octavian-Tiberiu NISTORA <octavian.nistora@gmail.com>
2024-12-02 16:25:01 +0200

    Changed websockethandler class to properly support multiple clients

--- Api/src/main/java/com/example/api/data/access/repositories/PlayerRepository.java
+++ Api/src/main/java/com/example/api/data/access/repositories/PlayerRepository.java
@@ -1,6 +1,7 @@
 package com.example.api.data.access.repositories;
 
 import com.example.api.data.access.entities.Player;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
